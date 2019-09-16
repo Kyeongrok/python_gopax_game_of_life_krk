e37b8245e67ab58f657ca1d364e89c55f5510ac3
kyeongrok <oceanfog1@gmail.com>
2019-09-16 18:52:05 +0900

    파일로 저장하는 기능 추가

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import sys
+from random import randrange
 from conway.gol import GameOfLife
 
 def parse_file(lines):
@@ -9,28 +10,44 @@ def parse_file(lines):
               "points":points}
     return result
 
+def get_random_command():
+    rows = randrange(40, 60)
+    cols = randrange(80, 100)
+    number_of_points = randrange(200, 300)
+
+    points = []
+    for i in range(number_of_points):
+        points.append({"x":randrange(1, rows - 2), "y":randrange(1, cols - 2)})
+    return {"num_rows":rows, "num_cols":cols, "points":points}
+
+
 def main():
-    # commad 입력 받음
     if len(sys.argv) == 2:
+        # commad 입력 받음
         file = open(sys.argv[1])
         lines = file.readlines()
         command = parse_file(lines)
-
-        gol = GameOfLife(command["num_rows"], command["num_cols"], command["points"])
-
-        while gol.state == "waiting":
-            key = gol.win.getch()
-            if key == ord("s"):
-                gol.start()
+        save_info = {"is_save":False, "target_generation":0}
+        gol = GameOfLife(command, save_info)
 
     elif len(sys.argv) == 3:
         # 두번째 파라메터의 횟수가 되었을 때 파일을 출력한다.
-        print("---파일 출력하기---")
+        file = open(sys.argv[1])
+        lines = file.readlines()
+        command = parse_file(lines)
+        save_info = {"is_save":True, "target_generation":int(sys.argv[2])}
+        gol = GameOfLife(command, save_info)
+
     else:
         # parameter가 없다는 것. 랜덤으로 생성한다.
-        print("---{}---", len(sys.argv))
-        gol = GameOfLife(10, 10, [])
-        gol.start()
+        command = get_random_command()
+        save_info = {"is_save":False, "target_generation":0}
+        gol = GameOfLife(command, save_info)
+
+    while gol.state == "waiting":
+        key = gol.win.getch()
+        if key == ord("s"):
+            gol.start()
 
 if __name__ == '__main__':
     main()
--- conway/gol.py
+++ conway/gol.py
@@ -7,15 +7,19 @@ class GameOfLife():
     grid = []
     generations = 0
     state = "waiting"
+    save_mode = False
+    file_saved = False
 
-    def __init__(self, num_rows, num_cols, points):
+    def __init__(self, command, save_info):
         self.stdscr = curses.initscr()
         self.stdscr.keypad(1)
+        self.save_mode = save_info["is_save"]
+        self.target_generation = save_info["target_generation"]
         curses.curs_set(0)
         curses.noecho()
         curses.cbreak()
-        self.height = max(40, num_rows)
-        self.width = max(80, num_cols)
+        self.height = max(40, command["num_rows"])
+        self.width = max(80, command["num_cols"])
         self.win = curses.newwin(self.height, self.width, 0, 0)
         self.win.nodelay(1)
         # self.win.move(1, 1)
@@ -23,7 +27,7 @@ class GameOfLife():
         self.draw_info()
 
         # 입력받은 점을 찍는다
-        for point in points:
+        for point in command["points"]:
             self.grid[point["x"]][point["y"]] = 1
 
     def get_next(self):
@@ -58,8 +62,10 @@ class GameOfLife():
 
     def draw_info(self):
         self.win.box()
-        self.win.addstr(2, self.height - 21, "generations: {}".format(self.generations))
-        self.win.addstr(3, self.height - 21, "s: start    q: quit")
+        self.win.addstr(1, self.height - 11, "generations: {}    save_mode:{}".format(self.generations, self.save_mode))
+        self.win.addstr(2, self.height - 11, "s: start    q: quit")
+        if self.save_mode == True and self.file_saved == True:
+            self.win.addstr(3, self.height - 11, "file saved to result.txt".format())
 
     def draw(self):
         # grid를 화면에 출력한다
@@ -68,6 +74,12 @@ class GameOfLife():
                 if self.grid[i][j] == 1:
                     self.win.addch(i, j, "+")
         self.win.refresh()
+
+    def save_n_generation_to_file(self, file_name):
+        file = open(file_name, "w+")
+        lines = [ str(line)+"\n" for line in self.grid]
+        file.writelines(lines)
+        file.close()
 
     def start(self):
         print("start")
@@ -78,8 +90,15 @@ class GameOfLife():
             self.draw_info()
             self.draw()
             time.sleep(0.1)
-            self.grid = self.get_next()
+
+            # 입력 받은 generation이 되면 파일로 저장한다.
+            if self.save_mode == True and self.target_generation == self.generations:
+                self.save_n_generation_to_file("./result.txt")
+                # 저장되었습니다 메세지 출력하기
+                self.file_saved = True
+
             key = self.win.getch()
             if key == ord("q"):
                 exit()
+            self.grid = self.get_next()
 
